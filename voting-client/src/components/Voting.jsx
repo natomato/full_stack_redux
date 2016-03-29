4941ae9df0d36137bf53475cef28fa37e38ad18b
Nate Tate <nate91711@gmail.com>
2016-03-28 21:54:04 -0700

    handle client side hasVoted state in reducer

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -26,6 +26,7 @@ export class Voting extends React.Component {
 function mapStateToProps(state) {
   return {
     pair: state.getIn(['vote', 'pair']),
+    hasVoted: state.get('hasVoted'),
     winner: state.get('winner')
   }
 }
--- voting-client/test/reducer.spec.js
+++ voting-client/test/reducer.spec.js
@@ -59,4 +59,86 @@ describe('reducer', () => {
       }
     }));
   });
+  it('handles SET_STATE removes hasVoted if the pair changes', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Snickers', 'Doodles'],
+        tally: {'Doodles': 1}
+      },
+      hasVoted: 'Doodles'
+    });
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Potato chips', 'Potatos']
+        }
+      }
+    };
+    const nextState = reducer(initialState, action);
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Potato chips', 'Potatos']
+      }
+    }));
+  });
+  it('handles SET_STATE keeps hasVoted if the pair stays', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Snickers', 'Doodles'],
+        tally: {'Doodles': 1}
+      },
+      hasVoted: 'Doodles'
+    });
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Snickers', 'Doodles'],
+          tally: {'Snickers': 1, 'Doodles': 1}
+        }
+      }
+    };
+    const nextState = reducer(initialState, action);
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Snickers', 'Doodles'],
+        tally: {'Snickers': 1, 'Doodles': 1}
+      },
+      hasVoted: 'Doodles'
+    }));
+  });
+  it('handles VOTE by setting hasVoted', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Snickers', 'Doodles'],
+        tally: {'Doodles': 1}
+      }
+    });
+    const action = { type: 'VOTE', entry: 'Doodles'};
+    const nextState = reducer(state, action);
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Snickers', 'Doodles'],
+        tally: {'Doodles': 1} //NOTE: server's responsibility to update
+      },
+      hasVoted: 'Doodles'
+    }));
+  });
+  it('handles VOTE by NOT setting hasVoted for invalid entries', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Snickers', 'Doodles'],
+        tally: {'Doodles': 1}
+      }
+    });
+    const action = { type: 'VOTE', entry: 'Ice Cream'};
+    const nextState = reducer(state, action);
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Snickers', 'Doodles'],
+        tally: {'Doodles': 1}
+      }
+    }));
+  });
 });
